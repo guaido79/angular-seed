c58ffad60ea1398a71444c97262b2ae8be9428c3
Guido Magnini <guido.magnini@infogroup.it>
2016-09-16 17:16:03 +0200

    XHR & Dependency Injection

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -8,25 +8,13 @@ angular.
     }])
     .component('phoneList', {
         templateUrl: 'phone-list/phone-list.template.html',
-        controller: function PhoneListController() {
+        controller: ['$http', function PhoneListController($http) {
             var self = this;
 
-            self.phones = [
-                {
-                    name: 'Nexus S',
-                    snippet: 'Fast just got faster with Nexus S.',
-                    age: 1
-                }, {
-                    name: 'Motorola XOOM™ with Wi-Fi',
-                    snippet: 'The Next, Next Generation tablet.',
-                    age: 2
-                }, {
-                    name: 'MOTOROLA XOOM™',
-                    snippet: 'The Next, Next Generation tablet.',
-                    age: 3
-                }
-            ];
-
             self.orderProp = 'age';
-        }
+
+            $http.get('phones/phones.json').then(function(response) {
+                self.phones = response.data;
+            });
+        }]
     });
