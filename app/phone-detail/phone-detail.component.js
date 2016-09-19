6456c65dec3ce2862c3a6a29140381610fda79d7
Guido Magnini <guido.magnini@infogroup.it>
2016-09-19 15:05:44 +0200

    12 - Event Handlers

--- app/phone-detail/phone-detail.component.js
+++ app/phone-detail/phone-detail.component.js
@@ -11,8 +11,13 @@ angular.
         controller: ['$routeParams', '$http', function PhoneDetailController($routeParams, $http) {
             var self = this;
 
+            self.setImage = function setImage(imageUrl) {
+                self.mainImageUrl = imageUrl;
+            };
+
             $http.get('phones/' + $routeParams.phoneId + '.json').then(function (response) {
                 self.phone = response.data;
+                self.setImage(self.phone.images[0]);
             });
         }]
     });
--- app/phone-detail/phone-detail.component.spec.js
+++ app/phone-detail/phone-detail.component.spec.js
@@ -1,28 +1,33 @@
-describe('phoneDetail', function() {
+describe('phoneDetail', function () {
 
-  // Load the module that contains the `phoneDetail` component before each test
-  beforeEach(module('phoneDetail'));
+    // Load the module that contains the `phoneDetail` component before each test
+    beforeEach(module('phoneDetail'));
 
-  // Test the controller
-  describe('PhoneDetailController', function() {
-    var $httpBackend, ctrl;
+    // Test the controller
+    describe('PhoneDetailController', function () {
+        var $httpBackend, ctrl;
+        var xyzPhoneData = {
+            name: 'phone xyz',
+            images: ['image/url1.png', 'image/url2.png']
+        };
 
-    beforeEach(inject(function($componentController, _$httpBackend_, $routeParams) {
-      $httpBackend = _$httpBackend_;
-      $httpBackend.expectGET('phones/xyz.json').respond({name: 'phone xyz'});
+        beforeEach(inject(function ($componentController, _$httpBackend_, $routeParams) {
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('phones/xyz.json').respond(xyzPhoneData);
 
-      $routeParams.phoneId = 'xyz';
+            $routeParams.phoneId = 'xyz';
 
-      ctrl = $componentController('phoneDetail');
-    }));
+            ctrl = $componentController('phoneDetail');
+        }));
 
-    it('should fetch the phone details', function() {
-      expect(ctrl.phone).toBeUndefined();
+        it('should fetch the phone details', function () {
+            expect(ctrl.phone).toBeUndefined();
 
-      $httpBackend.flush();
-      expect(ctrl.phone).toEqual({name: 'phone xyz'});
+            $httpBackend.flush();
+            expect(ctrl.phone).toEqual(xyzPhoneData);
+        });
+
+
     });
-
-  });
 
 });
